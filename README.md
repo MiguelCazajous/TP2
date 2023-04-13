79873e9f1db7d3ce32a270aec3f8e7a861dd13fe
Cazajous Miguel A <miguel.cazajous@unc.edu.ar>
2023-04-13 17:14:48 -0300

    Implementado procedimiento de consulta a API

--- README.md
+++ README.md
@@ -5,11 +5,6 @@
 - Marclé Emiliano
 - Garella Andrés
 
-## Description
-
-El proyecto es una calculadora de cotización de criptomonedas donde la capa superior recupera la cotización de dos criptomonedas (implementación en Python) de alguna REST API y envía los datos de la consulta al código escrito en C que convoca rutinas en ensamblador para hacer los cálculos de conversión.
-El resultado final es luego mostrado dentro del script en Python.
-
 ## Entorno
 
 Debido a que la librería en C está compilada para x86, debemos usar python compatible con esa arquitectura.
@@ -24,4 +19,13 @@ Para ese propósito se creó un entorno virtual (virtual env) usando `conda`
 
 - Activamos el entorno
 `conda activate TP2`
+
+## Description
+
+El proyecto es una simple calculadora de cotización de criptomonedas donde la capa superior recupera la cotización de dos criptomonedas (implementación en Python) de alguna REST API y envía los datos de la consulta al código escrito en C que convoca rutinas en ensamblador para hacer los cálculos de conversión.
+El resultado final es luego mostrado dentro del script en Python.
+
+La API REST elegida es financialmodelingprep, mediante un simple registro con una cuenta de gmail ya disponemos de un TOKEN para hacer requests, además de que la documentación es muy intuitiva, completa y proporciona la información necesaria para nuestra implementación.
+
+Más información: https://site.financialmodelingprep.com/developer/docs/
 
--- front.py
+++ front.py
@@ -1,15 +1,65 @@
 """
-docstring
+Cryptocurrency conversor frontend.
 """
-
 import ctypes
 import os
+import asyncio
+import aiohttp
+import json
+import sys
+
 
 libPath="lib"
 libName="libHelloWorld.so"
+apiUrlBase="https://financialmodelingprep.com/api/v3/quote/"
+symbols = [
+            "BTCUSD",
+            "ETHUSD",
+            "USDARS",
+            "USDEUR"
+          ]
+
+
+def getApiKey():
+    """
+        Gets API key from a file.
+    """
+    try:
+        with open("api_key", "r") as file:
+            return file.read().replace('\n','')
+    except IOError:
+        print("Could not get API key")
+        sys.exit(1)
+
+
+async def getResponse(session, apiUrl, apiKey):
+    """
+        Performs the http request to the API.
+    """
+    params = {'apikey': apiKey}
+    async with session.get(apiUrl, params=params) as response:
+        response = await response.json()
+        return response
+
+
+async def apiRequestsList(apiKey):
+    """
+        Stores in a list all the required requests.
+    """
+    async with aiohttp.ClientSession() as session:
+        requests=[]
+        for symbol in symbols:
+            apiUrl = apiUrlBase + symbol
+            requests.append(asyncio.ensure_future(getResponse(session, apiUrl, apiKey)))
+
+        responses = await asyncio.gather(*requests)
+        print(responses)
 
 
 def loadLibrary():
+    """
+        Loads a 32 bits library in C language.
+    """
     path=os.path.join(libPath, libName)
     libHelloWorld = ctypes.CDLL(path)
     #libHelloWorld.helloWorld.argtypes = (ctypes.c_int,)
@@ -18,11 +68,11 @@ def loadLibrary():
 
 
 def main():
-    """TODO: Docstring for main.
-    :returns: TODO
-
+    """
+        Main function.
     """
     loadLibrary()
+    asyncio.run(apiRequestsList(getApiKey()))
 
 
 if __name__ == "__main__":
