9280e5d8b7354c5227e971a46ba89fa2c74e2e75
Cazajous Miguel A <miguel.cazajous@unc.edu.ar>
2023-04-22 19:42:22 -0300

    Agregado simple código de testing

---
+++ lib/debug/debug.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include"../definitions.h"
+
+int main(int argc, char **argv){
+    double a = 3.5;
+    double b = 1.5;
+    double c = 0;
+
+    c = product(a,b);
+
+    return 0;
+}
+
